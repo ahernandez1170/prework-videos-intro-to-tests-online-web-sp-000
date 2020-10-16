cdbfb0cadb8d50982a931bbf146ae21a8c88e300
Austin Hernandez <ahernandez1170@gmail.com>
2020-10-16 19:39:48 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -4,7 +4,7 @@ describe "conversions" do
   describe '#ounces_to_grams' do
     it 'given zero, returns 3.4' do
       grams = ounces_to_grams(3.4)
-      expect(grams).to eq(3.4)
+      expect(grams).to eq(0)
     end
   end
 end
